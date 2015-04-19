19306a2191f53d9e19c03e73b8f7d92ae3aded81
pujariamol <pujari.s.amol@gmail.com>
2015-04-19 00:15:13 -0700

    Servlet added for initializing on server startup

---
+++ src/com/photoshare/utility/PhotoshareStartup.java
@@ -0,0 +1,26 @@
+package com.photoshare.utility;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+import com.photoshare.db.HibernateUtil;
+
+/**
+ * @author Amol
+ * 
+ * this class is used for initializing resources when the server starts
+ *
+ */
+public class PhotoshareStartup extends HttpServlet {
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		
+		//Creates db if the config is set to create else will perform the operation accordingly
+		//we will be performing this oepration here so it won't take much time when a first db 
+		// operation is performed.
+		HibernateUtil hibernateUtil = new HibernateUtil();
+		hibernateUtil.getSessionFactory();
+		
+	}
+}
