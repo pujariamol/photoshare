3f7ac53756a4a65f1b6ca77f465f010d05b34513
pujariamol <pujari.s.amol@gmail.com>
2015-04-19 16:22:41 -0700

    File formatted

--- src/com/photoshare/utility/Constants.java
+++ src/com/photoshare/utility/Constants.java
@@ -1,10 +1,9 @@
 package com.photoshare.utility;
 
+/**
+ * @author Amol
+ *
+ */
 public class Constants {
 
-	// JDBC driver name and database URL
-	public static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	public static final String DB_URL = "jdbc:mysql://localhost/";
-
-	
 }
--- src/com/photoshare/utility/PhotoshareStartup.java
+++ src/com/photoshare/utility/PhotoshareStartup.java
@@ -8,19 +8,21 @@ import com.photoshare.db.HibernateUtil;
 /**
  * @author Amol
  * 
- * this class is used for initializing resources when the server starts
- *
+ *         this class is used for initializing resources when the server starts
+ * 
  */
 public class PhotoshareStartup extends HttpServlet {
 	@Override
 	public void init() throws ServletException {
 		super.init();
-		
-		//Creates db if the config is set to create else will perform the operation accordingly
-		//we will be performing this oepration here so it won't take much time when a first db 
+
+		// Creates db if the config is set to create else will perform the
+		// operation accordingly
+		// we will be performing this oepration here so it won't take much time
+		// when a first db
 		// operation is performed.
 		HibernateUtil hibernateUtil = new HibernateUtil();
 		hibernateUtil.getSessionFactory();
-		
+
 	}
 }
