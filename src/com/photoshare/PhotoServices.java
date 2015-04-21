a6a23e7f18e8545c1ff8781ecf87dab19def5f89
Mahesh Bingi <mahesh.bingi@gmail.com>
2015-04-21 02:18:31 -0700

    - Photo upload functionality with jersey-multipart version 1.19 - Photo download (GET) functionality - Photo Delete with basic URI - Photo Update with basic URI and new Image

--- src/com/photoshare/PhotoServices.java
+++ src/com/photoshare/PhotoServices.java
@@ -1,15 +1,17 @@
 package com.photoshare;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Date;
+import java.util.UUID;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
@@ -19,6 +21,9 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.json.JSONException;
+
+import com.sun.jersey.api.client.ClientResponse.Status;
 import com.sun.jersey.core.header.FormDataContentDisposition;
 import com.sun.jersey.multipart.FormDataParam;
 import com.photoshare.dao.PhotoDAO;
@@ -33,63 +38,136 @@ import com.photoshare.model.PhotoMeta;
 @Path("/photos")
 public class PhotoServices {
 
+	private final String uploadFileHomeLocation = "/Users/maheshbingi/Documents/MS/Spring15/277/Project/images/";
+	
 	@POST
 	@Consumes(MediaType.MULTIPART_FORM_DATA)
-	public Response addPhoto(@FormDataParam("file") InputStream uploadedInputStream) {
+	public Response addPhoto(
+			@FormDataParam("file") InputStream uploadedInputStream,
+			@FormDataParam("file") FormDataContentDisposition fileDetail) {
  
-		String uploadedFileLocation = "/Users/maheshbingi/Documents/MS/Spring15/277/Project/abcd.jpg";
-		
-		// save it
-		writeToFile(uploadedInputStream, uploadedFileLocation);
-		String output = "File uploaded to : " + uploadedFileLocation;
-		 
-		return Response.status(200).entity(output).build(); 
-	}
-	
-	/*
-	public Response addPhoto(@FormDataParam("param1") String param) {
-	 
-		String output = "File uploaded to : " + param;
-		 
-		return Response.status(200).entity(output).build();
-		
-		
-		
-		
-		
+		String uniqueFileName = UUID.randomUUID() + "." + fileDetail.getFileName().split("\\.")[1];
+		String uploadedFileLocation = uploadFileHomeLocation + uniqueFileName;
 		
 		PhotoDAO photoDAO = new PhotoDAO();
 	 	
 		PhotoMeta photo = new PhotoMeta();
-		photo.setName("Test pic");
+		photo.setName(uniqueFileName);
 		photo.setDate(new Date());
-		photo.setLocation("San Jose");
-		photo.setDescription("Test description");
-		// SET image type (jpeg,png)
-		// SET imagePath
+		photo.setLocation(uploadedFileLocation);
+		photo.setURL(uploadedFileLocation);
+		photo.setDescription(fileDetail.getFileName().split("\\.")[0]);
 		photoDAO.insert(photo);
 		
-		// Read Bytes data image and form image and store on disk
-	
-	
+		// save file to disk
+		writeToFile(uploadedInputStream, uploadedFileLocation);
 		
-		System.out.println("---------------------------successfully inserted");
+		// form JSON response message
+		org.json.JSONObject responseObj = new org.json.JSONObject();
+		try {
+			responseObj.put("success", true);
+			responseObj.put("response-code", "201");
+			responseObj.put("fileURI", uploadedFileLocation);
+			responseObj.put("filename", uniqueFileName);
+			responseObj.put("message", "File uploaded successfully..");
+			
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		photo.setDescription("---------------------------Description changed");
+		return Response.status(201).entity(responseObj.toString()).build(); 
+	}
+	
+	@PUT
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	@Path("/{photoId}")
+	public Response updatePhoto(
+			@FormDataParam("file") InputStream uploadedInputStream,
+			@FormDataParam("file") FormDataContentDisposition fileDetail,
+			@PathParam("photoId") String uniqueFileName) {
+		
+		String uploadedFileLocation = uploadFileHomeLocation + uniqueFileName;
+		
+		PhotoDAO photoDAO = new PhotoDAO();
+	 	
+		PhotoMeta photo = new PhotoMeta();
+		photo.setName(uniqueFileName);
+		photo.setDate(new Date());
+		photo.setLocation(uploadedFileLocation);
+		photo.setURL(uploadedFileLocation);
+		photo.setDescription(fileDetail.getFileName().split("\\.")[0]);
 		photoDAO.update(photo);
-		System.out.println("---------------------------successfully updated");
 		
+		// save file to disk
+		writeToFile(uploadedInputStream, uploadedFileLocation);
+		
+		// form JSON response message
+		org.json.JSONObject responseObj = new org.json.JSONObject();
+		try {
+			responseObj.put("success", true);
+			responseObj.put("response-code", "200");
+			responseObj.put("fileURI", uploadedFileLocation);
+			responseObj.put("filename", uniqueFileName);
+			responseObj.put("message", "File updated successfully..");
+			
+		} catch (JSONException e) {
+			return Response.status(200).build();
+		}
+		
+		return Response.status(200).entity(responseObj.toString()).build();
+	}
+
+	@DELETE
+	@Path("/{photoId}")
+	public Response deletePhoto(@PathParam("photoId") String uniqueFileName) {
+		
+		PhotoDAO photoDAO = new PhotoDAO();
 		PhotoMeta rPhoto = photoDAO.getObject(PhotoMeta.class, 1);
 		
-		System.out.println("---------------------------Name = " + rPhoto.getName() + "\n Description = " + rPhoto.getDescription() );
-		
 		photoDAO.delete(rPhoto);
-		System.out.println("---------------------------Record deleted successfully");
-	 
 		
-		return "test";
+		// form JSON response message
+		org.json.JSONObject responseObj = new org.json.JSONObject();
+		try {
+			responseObj.put("success", true);
+			responseObj.put("response-code", "200");
+			responseObj.put("message", "File deleted successfully..");
+			
+		} catch (JSONException e) {
+			return Response.status(200).build();
+		}
+		
+		return Response.status(200).entity(responseObj.toString()).build();
 	}
-	*/
+
+	@GET
+	@Path("/{photoId}")
+	@Produces("image/*")
+	public Response getPhoto(@PathParam("photoId") String image) {
+
+		File f = new File(uploadFileHomeLocation+image);
+		
+		org.json.JSONObject responseObj = new org.json.JSONObject();
+		
+		if(!f.exists()) {
+			try {
+				responseObj.put("success", false);
+				responseObj.put("response-code", "404");
+				responseObj.put("message", "File not found..");
+				return Response.status(404).entity(responseObj.toString()).build();
+			} catch (JSONException e) {
+				return Response.status(Status.NOT_FOUND).build();
+			}
+			
+		}
+
+		try {
+	        return Response.ok(new FileInputStream(f)).build();
+	    } catch (FileNotFoundException e) {
+	        return Response.status(Status.NOT_FOUND).build();
+	    }
+	}
 	
 	// save uploaded file to new location
 	private void writeToFile(InputStream uploadedInputStream,
@@ -111,40 +189,5 @@ public class PhotoServices {
  
 			e.printStackTrace();
 		}
- 
 	}
-
-	@PUT
-	@Produces(MediaType.TEXT_PLAIN)
-	@Path("/{photoId}")
-	public String updatePhoto(@PathParam("photoId") int photoId) {
-		return "test + " + photoId;
-	}
-
-	@DELETE
-	@Path("/{photoId}")
-	public void deletePhoto(@PathParam("photoId") int photoId) {
-
-	}
-
-	@GET
-	@Path("/{photoId}")
-	@Consumes(MediaType.MULTIPART_FORM_DATA)
-	public Response getPhoto() {
-
-		String output = "Called GET with ID: ";
-		
-		return Response.status(200).entity(output).build();
-	}
-	
-	@POST
-	@Path("/photoId")
-	@Consumes(MediaType.MULTIPART_FORM_DATA)
-	public Response getPostPhoto(@FormDataParam("photoId") int photoId) {
-
-		String output = "Called GET with ID: "+ photoId;
-		
-		return Response.status(200).entity(output).build();
-	}
-
 }
