06626f99bf6a0119c1daee4a93df50b557c899bf
pujariamol <pujari.s.amol@gmail.com>
2015-04-23 19:20:14 -0700

    New DTO class added for setting different fields in response

---
+++ src/com/photoshare/dto/ResponseDTO.java
@@ -0,0 +1,56 @@
+package com.photoshare.dto;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.json.JSONObject;
+
+/**
+ * @author Amol
+ *
+ */
+
+
+public class ResponseDTO {
+
+	private boolean success = false;
+	private String message = "";
+
+	public ResponseDTO() {
+
+	}
+
+	/**
+	 * @param success
+	 *            boolean value depending on the result of the response
+	 * @param message
+	 *            string parameter for setting message related to the operation
+	 *            like success message or error details
+	 * 
+	 */
+	public ResponseDTO(boolean success, String message) {
+		this.success = success;
+		this.message = message;
+	}
+
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return new JSONObject(this).toString();
+	}
+
+}
