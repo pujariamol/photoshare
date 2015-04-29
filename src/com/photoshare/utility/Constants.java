8593bb0b4df3c76f8038dfbd8a708ae37c960f47
pujariamol <pujari.s.amol@gmail.com>
2015-04-29 00:35:04 -0700

    new constants for application

--- src/com/photoshare/utility/Constants.java
+++ src/com/photoshare/utility/Constants.java
@@ -10,4 +10,11 @@ public class Constants {
 	public static final String ALBUM_UPDATED_SUCCESS = "Album updated successfully!!";
 	public static final String ALBUM_NOT_FOUND = "Album does not exists!!";
 	
+	public static final String USER_FETCHED_SUCCESSFULLY = "User fetched successfully!!";
+	public static final String USER_DOES_NOT_EXISTS = "User does not exists!!";
+	public static final String PHOTOS_ALBUM_FETCHED_SUCCESSFULLY = "Photos fetched from album successfully!!";
+	
+	public static final String USER_ALBUMS_FETCHED_SUCCESSFULLY = "User albums fetched successfully!!";
+	
+	
 }
